ed0884f87a1f68bb75994ae02b23b3a685c3cd23
Anusha Gaargya <anushasgarg@gmail.com>
2023-10-04 17:46:26 +0530

    headlines and description views

--- NewsApp/ListView.swift
+++ NewsApp/ListView.swift
@@ -9,56 +9,98 @@
 import SwiftUI
 import SwiftyJSON
 import SDWebImageSwiftUI
-
+import WebKit
 
 struct ListView: View {
+    @ObservedObject var list = getData()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ListView()
-    }
-}
-
-struct dataType: Identifiable {
-    var id: String
-    var title: String
-    var desc: String
-    var url: String
-    var image: String
-}
-
-class getData: ObservableObject {
-    @Published var datas = [dataType]()
-    init() {
-        let source = "https://newsapi.org/v2/top-headlines?country=us&apiKey=1d233c9f461a4f2e97497de7d452222b"
-        
-        let url = URL(string: source)!
-        
-        let session = URLSession(configuration: .default)
-        session.dataTask(with: url) {
-            (data, _, err) in
-            if err != nil {
-                print(err!.localizedDescription)
-                return
-            }
-            
-            let json = try! JSON(data: data!)
-            
-            for i in json["articles"]{
-                let title = i.1["title"].stringValue
-                let description = i.1["description"].stringValue
-                let url  = i.1["url"].stringValue
-                let image = i.1["urlToImage"].stringValue
-                let id = i.1["publishedAt"].stringValue
-                
-                self.datas.append(dataType(id: id, title:title, desc: description, url: url, image: image))
-            }
+        NavigationView{
+            List($list.datas){ i in
+                NavigationLink(destination:
+                                webView(url: i.url.wrappedValue).navigationBarTitle("", displayMode: .inline)){
+                    HStack(spacing:15){
+                        VStack(alignment: .leading, spacing: 10){
+                            
+                            Text(i.title.wrappedValue).fontWeight(.heavy)
+                            Text(i.desc.wrappedValue).lineLimit(2)
+                            
+                        }
+                        
+                        if i.image.wrappedValue != ""{
+                        
+                            WebImage(url: URL(string: i.image.wrappedValue), options: .highPriority, context: nil)
+                                .resizable()
+                                .cornerRadius(5)
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }.padding(.vertical, 15)
+                }
+               
+            }.navigationTitle("Headlines")
         }
     }
+    
+    struct ListView_Previews: PreviewProvider {
+        static var previews: some View {
+            ListView()
+        }
+    }
+    
+    struct dataType: Identifiable {
+        var id: String
+        var title: String
+        var desc: String
+        var url: String
+        var image: String
+    }
+    
+    class getData: ObservableObject {
+        @Published var datas = [dataType]()
+        init() {
+            let source = "https://newsapi.org/v2/top-headlines?country=in&apiKey=1d233c9f461a4f2e97497de7d452222b"
+            
+            let url = URL(string: source)!
+            
+            let session = URLSession(configuration: .default)
+            session.dataTask(with: url) {
+                (data, _, err) in
+                if err != nil {
+                    print(err!.localizedDescription)
+                    return
+                }
+                
+                let json = try! JSON(data: data!)
+                
+                for i in json["articles"]{
+                    let title = i.1["title"].stringValue
+                    let description = i.1["description"].stringValue
+                    let url  = i.1["url"].stringValue
+                    let image = i.1["urlToImage"].stringValue
+                    let id = i.1["publishedAt"].stringValue
+                    DispatchQueue.main.async {
+                        self.datas.append(dataType(id: id, title:title, desc: description, url: url, image: image))
+                    }
+                    
+                }
+            }.resume()
+        }
+    }
+    
 }
+    
+    struct webView : UIViewRepresentable {
+        var url : String
+        func makeUIView(context: UIViewRepresentableContext<webView>) -> WKWebView{
+            let view = WKWebView()
+            view.load(URLRequest(url: URL(string: url)!))
+            return view
+            
+        }
+        func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<webView>) {
+            UIViewRepresentableContext<webView>.self
+        }
+        
+    }
+
 //GENERATE API KEY in NEWSAPI
 
